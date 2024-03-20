12c39cc4f36811726d14097ba993b728f345b29c
Ande <andesec1@gmail.com>
2024-03-20 22:20:30 +0500

    some refactoring and improvements to select the files to process

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ import time
 import argparse
 import subprocess
 import re
+import glob
 
 from urllib.parse import urljoin
 from datetime import datetime
@@ -111,7 +112,16 @@ def write_video(input_file, output_file, total_duration):
 
     with open(ffmpeg_log_filepath, 'w') as log_file:
         process = subprocess.Popen(
-            ["ffmpeg", "-f", "concat", "-safe", "0", "-i", input_file, "-c", "copy", output_file],
+            [
+                "ffmpeg",
+                "-analyzeduration", "10000000",
+                "-probesize", "10000000",
+                "-f", "concat",
+                "-safe", "0",
+                "-i", input_file,
+                "-c", "copy",
+                output_file
+            ],
             stdout=subprocess.PIPE,
             stderr=subprocess.STDOUT,
             universal_newlines=True
@@ -172,73 +182,87 @@ csv_dir_path = os.path.join(
     'csv'
 )
 
-if __name__ == "__main__":
-    # Set up command-line argument parsing
+
+def parse_args():
     parser = argparse.ArgumentParser(description='Download videos from a CSV file.')
-    parser.add_argument('csv_filepath', help='The filepath of the CSV file.')
+    parser.add_argument('--csv_filepath', help='The filepath of the CSV file.')
     parser.add_argument('--output_dir', help='The directory where the output file should be saved.')
     parser.add_argument('--playlist_index', type=int, default=0, help='The index of the playlist to use.')
     parser.add_argument('--output-extension', type=str, default='mp4', help='The extension or format for this video.')
-
-    # Parse the command-line arguments
     args = parser.parse_args()
+    return args
 
-    # Provide the filepath for the CSV file
-    csv_filepath = args.csv_filepath
-    ext = args.output_extension
 
-    # Parse the CSV filename and create necessary directories
-    filename = os.path.basename(csv_filepath)
+def setup_directories(args, selected_csv_filepath):
+    filename = os.path.basename(selected_csv_filepath)
     first_part, second_part, *_ = filename.split('-')
-
     output_dir = args.output_dir
+
     if output_dir is None:
         with open('default_output_path.txt', 'r') as f:
             output_dir = f.read().strip()
 
-    # Ensure the output directory exists
-    # os.makedirs(output_dir, exist_ok=True)
     os.makedirs(os.path.join(output_dir, first_part, second_part), exist_ok=True)
+    return os.path.join(output_dir, first_part, second_part)
 
-    print_and_log(f"Process started at: {datetime.now().strftime('%Y-%m-%d %H:%M:%S')}")
-    print_and_log(f"CSV file: {csv_filepath}")
-    print_and_log(f"Output directory: {output_dir}")
-    print_and_log(f"Playlist index: {args.playlist_index}")
+
+def select_csv(args):
+    csv_filepath = args.csv_filepath
+    if csv_filepath is not None:
+        return csv_filepath
+
+    csv_files = glob.glob('csv/*.csv')
+    for i, csv_file in enumerate(csv_files, start=1):
+        print(f"{i}: {csv_file}")
+
+    while True:
+        selection = input("Enter the number of the CSV file to process: ")
+        if selection.isdigit() and 1 <= int(selection) <= len(csv_files):
+            return csv_files[int(selection) - 1]
+        else:
+            print("Invalid selection. Please try again.")
+
+
+def process_selected_csv(args, output_dir, csv_filepath):
+    ext = args.output_extension
     print_and_log(f"Output extension: {ext}")
-    print_and_log(f"Standard log file: {std_log_filepath}")
-    print_and_log(f"FFmpeg log file: {ffmpeg_log_filepath}")
 
-    # Parse the CSV file and download the videos
-    with open(f"csv/{csv_filepath}", 'r') as f:
+    with open(csv_filepath, 'r') as f:
         reader = csv.reader(f)
+
         for row in reader:
-            # Skip any row that doesn't have value in first or second column
             if not row or len(row) < 2 or not row[0] or not row[1]:
                 print_and_log(f"Invalid row: {row}")
                 continue
 
-            # Create the output filepath
-            output_filename = os.path.join(output_dir, first_part, second_part, f"{row[0]}.{ext}")
+            output_filename = os.path.join(output_dir, f"{row[0]}.{ext}")
             print_and_log(f"Downloading {row[0]} video to: {output_filename}")
-
-            # Get the m3u8 URL
             m3u8_url = row[1]
-
-            # Download the video and save it to the output file
             start_time = time.time()
 
             try:
-                download_m3u8_video(
-                    m3u8_url,
-                    output_filename,
-                    args.playlist_index  # Change the last parameter to select the playlist
-                )
+                download_m3u8_video(m3u8_url, output_filename, args.playlist_index)
             except Exception as e:
                 print_and_log(f"Failed to download video {row[0]}: {e}")
                 continue
 
             end_time = time.time()
             print_and_log("Time taken to download and process: {}", end_time - start_time)
+            print_and_log("----------------------------------------------------")
 
+
+if __name__ == "__main__":
+    args = parse_args()
+    selected_csv_filepath = select_csv(args)
+    output_dir = setup_directories(args, selected_csv_filepath)
+
+    print_and_log(f"Process started at: {datetime.now().strftime('%Y-%m-%d %H:%M:%S')}")
+    print_and_log(f"CSV file: {selected_csv_filepath}")
+    print_and_log(f"Output directory: {output_dir}")
+    print_and_log(f"Playlist index: {args.playlist_index}")
+    print_and_log(f"Standard log file: {std_log_filepath}")
+    print_and_log(f"FFmpeg log file: {ffmpeg_log_filepath}")
+
+    process_selected_csv(args, output_dir, selected_csv_filepath)
     print_and_log("Videos downloaded successfully!")
     print_and_log(f"Process ended at: {datetime.now().strftime('%Y-%m-%d %H:%M:%S')}")
