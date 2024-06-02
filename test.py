2ab4b443c631ebfc7d6beefc672c28205c7b1a61
Ande <andesec1@gmail.com>
2024-06-02 17:12:10 +0500

    test file

--- test.py
+++ test.py
@@ -15,7 +15,51 @@ def get_codec(url):
     return result.stdout.decode().strip()
 
 
+# Write another function uses ffmepg to get the FPS for the video
+def get_fps(url):
+    command = [
+        "ffprobe",
+        "-v", "error",
+        "-select_streams", "v:0",
+        "-show_entries", "stream=r_frame_rate",
+        "-of", "default=noprint_wrappers=1:nokey=1",
+        url
+    ]
+
+    result = subprocess.run(command, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+    return result.stdout.decode().strip()
+
+
+# write another function to get the name of the  video
+def get_name(url):
+    command = [
+        "ffprobe",
+        "-v", "error",
+        "-select_streams", "v:0",
+        "-show_entries", "format=filename",
+        "-of", "default=noprint_wrappers=1:nokey=1",
+        url
+    ]
+
+    result = subprocess.run(command, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+    return result.stdout.decode().strip()
+
+
 # Example usage:
 # url = "https://govbw.vid109d224.site/_v2-wwdo/12a3c523fb105800ed8c394685aeeb0b932efd5c02bdb6b04b187baea93ece832257df1a4b6125fcfa38c35da05dee86aad28d46d73fc4e9d4e5a13b5572f1d6749544f85d4eea0d12c5f7ec7c422b4a6366d373445766c1d99ab8008de77ec76657fe1d5336eb05beec3107f41c059a/h/list;15a38634f803584ba8926411d7bee906856cab0654b5b8.m3u8"
-url = "https://pnagb.vid109d224.site/_v2-oanz/12a3c523fb105800ed8c394685aeeb0b962efe5c5eb0ffba49417baea93ece832257df1a4b6125fcfa38c35da05dee86aad28d46d73fc4e9d4e5a4385375f3d7318246f6054feb4d4194afeb3c167a136362d13410043fc89ec5f7138cf97e997f40a34f5b67fc14b6aa61/h/list;15a38634f803584ba8926411d7bee906856cab0654b5b8.m3u8"
+url = "https://bcbolte44808b7-a.akamaihd.net/media/v1/dash/live/cenc/6015698128001/f73660d0-e533-42a6-9faa-98595fb2e910/bfe6aa75-5c30-48a9-a010-1d263bb97722/b53e305d-4f38-4919-8fcc-0726d00a6412/init.m4f?akamai_token=exp=1713323070~acl=/media/v1/dash/live/cenc/6015698128001/f73660d0-e533-42a6-9faa-98595fb2e910/bfe6aa75-5c30-48a9-a010-1d263bb97722/*~hmac=c6b584da8a259164fcb72ca466ea74018fc6c98eda0a3db8b1861211956a5e83"
+# url = "https://manifest.prod.boltdns.net/manifest/v1/dash/live-baseurl/bccenc/6015698128001/f73660d0-e533-42a6-9faa-98595fb2e910/6s/manifest.mpd?fastly_token=NjYxZjNjMGNfYjY2OTM0OTAzMmY3ZWM5MmYxOTM2ODJjNzMyZDlmNzk4MmMyNDhhNzkxNzI2NjE5YzU1YjJjZTNlZmRjYTJiMw%3D%3D"
 print(get_codec(url))
+print(get_fps(url))
+print(get_name(url))
+
+'''
+You can run the script above to get the codec and FPS of the video. The URL is the same as the one you provided. The codec is `h264` and the FPS is `25/1`.
+You can also use the `ffprobe` command to get other information about the video stream, such as resolution, bitrate, etc. You can refer to the `ffprobe` documentation for more details.
+Hope this helps!
+```
+$ python test.py
+h264
+25/1
+```
+'''
